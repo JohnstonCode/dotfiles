346313a4f7091f3d37440e7c7b83e417ce0875dc
Chris Johnston <chris.johnston@sport80.com>
2020-09-01 15:39:42 +0100

    Updated fish.config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 alias vim='nvim'
+alias up='sudo dnf update --refresh'
 
+# This is needed to help save configs with git https://www.atlassian.com/git/tutorials/dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
