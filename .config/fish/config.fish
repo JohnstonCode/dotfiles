6fab8ca87587f959edf0e161a621c383c244d050
Chris Johnston <chris.johnston@sport80.com>
2020-09-01 16:15:45 +0100

    Added exa alis to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 alias vim='nvim'
 alias up='sudo dnf update --refresh'
+alias ls='exa'
 
 # This is needed to help save configs with git https://www.atlassian.com/git/tutorials/dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
