1a1438f27039d60986f016c96df7746f2e78fd1e
Chris Johnston <chris.johnston@sport80.com>
2020-09-01 16:31:45 +0100

    added custom fish greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,11 @@ alias ls='exa'
 
 # This is needed to help save configs with git https://www.atlassian.com/git/tutorials/dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+function fish_greeting
+	echo
+	echo -e (cat /etc/fedora-release | awk '{print "\\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uptime -p | sed 's/^up //' | awk '{print "\\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (hostname -I | cut -d' ' -f1 | awk '{print "\\\\e[1mIP: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo
+end
