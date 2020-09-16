14990ab96b0d92759979fe69d9cdc685dbc8fbe8
Chris Johnston <chris.johnston@sport80.com>
2020-09-16 11:21:51 +0100

    added docker-compose alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 alias vim='nvim'
 alias up='sudo dnf update --refresh'
 alias ls='exa'
+alias dc='docker-compose'
 
 # This is needed to help save configs with git https://www.atlassian.com/git/tutorials/dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
