6bc70e54020b09308e6e674650087618803dfdee
Chris Johnston <chris.johnston@sport80.com>
2021-05-06 14:50:18 +0100

    Updated fish config for popOS

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,13 @@
-alias vim='nvim'
-alias up='sudo dnf update --refresh'
+alias up='sudo apt update && sudo apt upgrade'
 alias ls='exa -al --color=always --group-directories-first'
-alias dc='COMPOSE_DOCKER_CLI_BUILD=1 docker-compose'
+alias dc='docker-compose'
 
 # This is needed to help save configs with git https://www.atlassian.com/git/tutorials/dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 function fish_greeting
 	echo
-	echo -e (cat /etc/fedora-release | awk '{print "\\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (lsb_release -d --short | awk '{print "\\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
 	echo -e (uptime -p | sed 's/^up //' | awk '{print "\\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
 	echo -e (hostname -I | cut -d' ' -f1 | awk '{print "\\\\e[1mIP: \\\\e[0;32m"$0"\\\\e[0m"}')
 	echo
